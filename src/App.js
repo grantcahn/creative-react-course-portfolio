8d0912c5620c781743e943e898de242e86639b31
Grant <g_cahn@hotmail.com>
2022-04-15 09:12:53 +0200

    2. About section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+// import pages
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>film</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+//page components
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
