83559c10e682bd623f3e4ecc0798f95633a9e0ee
Grant <g_cahn@hotmail.com>
2022-04-15 10:14:50 +0200

    6 services styling

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,9 +10,12 @@ const GlobalStyle = createGlobalStyle`
 
   body{
       background: #1b1b1b;
+      font-family: 'Inter', sans-serif;
+/*font-family: 'Lobster', cursive; */
   }
   button{
       font-weight: bold;
+      font-family: 'Inter', sans-serif;
       font-size: 1.1rem;
       cursor: pointer;
       padding: 1rem 2rem;
