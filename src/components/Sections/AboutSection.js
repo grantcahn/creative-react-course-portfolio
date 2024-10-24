2f550afe78bde457f159898b382d97ce1d103513
grantcahn <grantcahn@gmail.com>
2024-10-24 16:31:16 +0200

    update images

--- src/components/Sections/AboutSection.js
+++ src/components/Sections/AboutSection.js
@@ -4,41 +4,77 @@ import { GenericSection, Section } from '../../styles'
 import { useScroll } from '../useScroll'
 import { fade } from '../../animation'
 import SectionTitle from '../SectionTitle'
+import animoca from '../../img/animocabrands.svg'
+import euroleague from '../../img/EL-logo.svg'
+import qpr from '../../img/icons/qpr-icon.png'
+import afl from '../../img/icons/AFL.png'
+import gresini from '../../img/gresini-logo.png'
+import sportpass from '../../img/sportpass-logo.svg'
+
+const brands = [
+  {
+    name: 'Animoca Brands',
+    image: animoca,
+  },
+  {
+    name: 'Euroleague',
+    image: euroleague,
+  },
+  {
+    name: 'QPR',
+    image: qpr,
+  },
+  {
+    name: 'AFL',
+    image: afl,
+  },
+  {
+    name: 'Gresini MotoGP',
+    image: gresini,
+  },
+  {
+    name: 'SPORTPASS',
+    image: sportpass,
+  },
+]
 
 const AboutSection = () => {
   const [element, controls] = useScroll()
   return (
     <AboutMe variants={fade} animate={controls} initial="hidden" ref={element}>
-      <SectionTitle title={'About Me'} />
-      <Section>
-        <SubSection>
-          <h2>I’ve been developing websites since 2018</h2>
-          <p>
-            What started as a hobby turned into a passion. I have been creating
-            websites for the past 6 years. I have worked with various clients
-            and have helped them to improve their online presence. I can help
-            you to create a professional website for your business.
-          </p>
-        </SubSection>
-        <SubSection>
-          <div className="counter-section">
-            <Counter>
-              <h1>6</h1>
-              <h3>Years of experience</h3>
-            </Counter>
-            <Counter>
-              <h1>10</h1>
-              <h3>Successful Projects</h3>
-            </Counter>
-          </div>
-          <p>
-            I started with good old HTML and CSS, WordPress and then moved on to
-            using React, Next.js and Svelte. Styling disciplines such as I have
-            mainly been responsible for the front-end development of websites. I
-            have also worked with back-end technologies like Nodejs and MongoDB.
-          </p>
-        </SubSection>
-      </Section>
+      <div className="aboutme">
+        <SectionTitle title={'About Me'} />
+        <div className='section-grid'>
+          <SubSection>
+            <h2>I’ve been developing websites since 2018</h2>
+            <p>
+              What started as a hobby turned into a passion. I have been creating
+              websites for the past 6 years. I have worked with various clients
+              and have helped them to improve their online presence. I can help
+              you to create a professional website for your business.
+            </p>
+          </SubSection>
+          <SubSection>
+            <div className="counter-section">
+              <Counter>
+                <h1>6</h1>
+                <h3>Years of experience</h3>
+              </Counter>
+              <Counter>
+                <h1>10</h1>
+                <h3>Successful Projects</h3>
+              </Counter>
+            </div>
+            <p>
+              I started with good old HTML and CSS, WordPress and then moved on to
+              using React, Next.js and Svelte. Styling disciplines such as I have
+              mainly been responsible for the front-end development of websites. I
+              have also worked with back-end technologies like Nodejs and MongoDB.
+            </p>
+          </SubSection>
+        </div>
+      </div>
+        <div className='brand-section'><p>Brands I've worked with</p><div className="brands"></div></div>
     </AboutMe>
   )
 }
@@ -46,6 +82,11 @@ const AboutSection = () => {
 //Styled Components
 const AboutMe = styled(GenericSection)`
   background-color: var(--color-bg2);
+  section-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 8rem;
+  }
   h2 {
     padding-bottom: 1rem;
     font-weight: 600;
